c53c703690116f78d1c490de9e6e7d7215eb76b0
Abydzar <ukrabka@gmail.com>
2026-05-05 10:25:43 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,73 @@
-#include <stdio.h>
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
+ *   Modul               : 4 Dynamic Structures
+ *   Hari dan Tanggal    : Selasa, 5 Mei 2026
+ *   Nama (NIM)          : Abydzar Dzakyan Akbar (13224106)
+ *   Nama File           : soal1.c
+ *   Deskripsi           : Menentukan ke validan simbol kurung dengan mengabaikan huruf kecil pada segel mantra 
+ * 
+ */
 
-int main(void) {
-    return 0;
-}
+ #include <stdio.h>
+ #include <string.h>
+ #include <stdlib.h>
+
+ typedef struct Node {
+    char data;
+    struct Node* next;
+ } Node;
+
+ void push (Node** head, char x){
+    Node* baru = (Node*)malloc(sizeof(Node));
+    baru->data = x;
+    baru->next = *head;
+    *head = baru;
+ }
+
+ char pop(Node** head) {
+    if (*head == NULL){
+        return '\0';
+    }
+    Node* temp = *head;
+    char x = temp->data;
+    *head = (*head)->next;
+    free(temp);
+    return x;
+ }
+  
+ int main() {
+    Node* data = NULL; //dalam stack
+    char input[999];
+    int valid;
+
+    scanf("%s", input);
+    for(int i=0; i<strlen(input); i++){
+        char dat = input[i];
+
+        if(dat == '(' || '[' || '{'){
+            push(&data, dat);
+        } else if(dat == ')' || ']' || '}'){
+            char buka_kurung = pop(&data);
+            if ((dat == ')' && buka_kurung != '(') || (dat == ']' && buka_kurung != '[') || (dat == '}' && buka_kurung =! '{')){
+                valid = 0;
+                break;
+            }
+        }
+
+        if (data == NULL){
+            printf("VALID");
+        }
+
+        if (valid = 1){
+            printf("VALID");
+        } else{
+            printf("INVALID");
+        }
+
+        return 0;
+    }
+
+
+
+
+  
+ }
